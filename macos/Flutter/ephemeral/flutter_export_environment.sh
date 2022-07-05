98b25e299d61db0525d26175ccb2e5dbc64f4304
Gibrán Vázquez <gibran.vazquez@gartic-consultores.com>
2022-07-05 13:37:42 -0500

    versión 1 para subir a Appstore

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/matiasfonseca/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/matiasfonseca/Desktop/easyAdapt/easy"
+export "FLUTTER_ROOT=/Users/gibranvs/desktop/gartic/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gibranvs/Desktop/GARTIC/Easy Adapt/Flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
